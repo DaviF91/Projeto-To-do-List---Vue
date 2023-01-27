91d02d598e6baa5910fe831d8a3d575ae1a8688b
Davi Ferreira <106997082+DaviF91@users.noreply.github.com>
2023-01-27 15:33:36 -0300

    v3

--- vue.js
+++ vue.js
@@ -1,7 +1,7 @@
 let id = 0
-let id1 = 0
 
 new Vue({
+    
     el:"#lista",
     data: {
         styleSelect: {
@@ -9,9 +9,11 @@ new Vue({
         },
         
         styleOption: {
-            color: ""
+            color: "",
+            
         },
         
+        opcao: "",
         opcoes: [
             {opcao:"A fazer"},
             {opcao:"Em andamento"},
@@ -21,70 +23,73 @@ new Vue({
         novaTarefa: "",  
         tarefas: [],
 
-        // editaTarefa: "",
-        tarefaSelecionada: []
-
+        tarefaSelecionada: [],
     },
     methods: {
         salvarTarefa(){
-            //nova condição != null
             if (this.novaTarefa == "") {
                 alert("Por favor digite uma tarefa")
             } 
             else if (this.novaTarefa.length > 100) {
                 alert ("A tarefa não pode ter mais que 100 caracteres")
             } 
-            // else if (this.novaTarefa == this.editaTarefa) {
-            //     this.tarefas.push({id: "", status: this.opcao, texto: this.editaTarefa})
-            //     this.novaTarefa = ""
-            //     this.styleSelect.display = "none"
-            //     this.colorOption(this.tarefaSelecionada)
-            // }
             else{
-                if (this.styleSelect.display == "none"){
+                if (this.styleSelect.display == "none" ){
                     this.opcao = "A fazer"
-                    //atribuir já a cor
                     this.styleOption.color = "red"
                 }
+                else if(this.opcao == "Em andamento"){
+                    this.styleOption.color = "yellow"
+                }
+                else if(this.opcao == "Finalizado"){
+                    this.styleOption.color = "green"
+                }
+                
                 this.tarefas.push({id: id++, status: this.opcao, texto: this.novaTarefa})
             } 
             this.novaTarefa = ""
             this.styleSelect.display = "none"
-            this.colorOption()
         },
 
+        // colorOption(){
+        //     // for (var i = 0; i < this.tarefaSelecionada.length; i++){
+                
+        //     //     if (statusTarefa[i] == "A fazer"){
+        //     //         this.styleOption.color = "red"           
+        //     //     } 
+        //     //     if (statusTarefa[i] == "Em andamento"){
+        //     //         this.styleOption.color = "yellow" 
+        //     //     }
+        //     //     if (statusTarefa[i] == "Finalizado"){
+        //     //         this.styleOption.color = "green" 
+        //     //     }
+        //     // }
+        //      Sugestão
+        //     var statusTarefa = this.tarefaSelecionada.map(i => i.status)
+        //     if (statusTarefa == "A fazer") {
+        //         this.styleOption.color = "red"
+        //     }
+        //     if (statusTarefa == "Em andamento") {
+        //         this.styleOption.color = "yellow"
+        //     }
+        //     if (statusTarefa == "Finalizado") {
+        //         this.styleOption.color = "green"
+        //     }
+        // },
         
         editarTarefa(tarefa){
             this.styleSelect.display = "" 
             this.novaTarefa = tarefa.texto
-            // this.editaTarefa = this.novaTarefa
             this.tarefas = this.tarefas.filter(i => i !== tarefa)
             
-            this.tarefaSelecionada = tarefa
+
+            // this.tarefaSelecionada = tarefa
         },
-        
+
         deletarTarefa(tarefa){
             let valida = prompt("Deseja realmente deletar essa tarefa ?")
             if (valida == "Sim"){
                 this.tarefas = this.tarefas.filter(i => i !== tarefa)
-            }
-        },
-        
-        colorOption(){
-            var statusTarefa = this.tarefas.map(i => i.status)
-            
-    
-            for (var i = 0; i < this.tarefas.length; i++){
-                
-                if (statusTarefa[i] == "A fazer"){
-                    this.styleOption.color = "red"           
-                } 
-                if (statusTarefa[i] == "Em andamento"){
-                    this.styleOption.color = "yellow" 
-                }
-                if (statusTarefa[i] == "Finalizado"){
-                    this.styleOption.color = "green" 
-                }
             }
         },
     }
