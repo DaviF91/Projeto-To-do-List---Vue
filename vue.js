df8120a33a223b9ebd7c4130c7202265eaeb03c3
Davi Ferreira <106997082+DaviF91@users.noreply.github.com>
2023-01-30 15:26:22 -0300

    projeto v6
    Inserido os itens abaixo na aplicação
    8- Colocar data na tarefa
      Permitir que seja possível adicionar uma data final para realização da tarefa. Sendo este campo opcional.
      Exibir a data na listagem e caso a data tenha passado (expirou), colocar ela em vermelho.
    
    9- Ordenar as "feitas" por último
      Ao colocar uma tarefa como "Feita", jogar ela para o final da lista, ordenando assim todas "feitas" para o final da lista.

--- vue.js
+++ vue.js
@@ -1,3 +1,5 @@
+
+
 let id = 0
 
 new Vue({
@@ -18,6 +20,9 @@ new Vue({
         idEditar: -1,
         novaTarefa: "",  
         tarefas: [],
+        
+        dataTarefa: "",
+        dataAtual:"",
     },
     methods: {
         salvarTarefa(idTarefa){
@@ -34,23 +39,27 @@ new Vue({
 
                 var novaTarefa = this.novaTarefa
                 var novaOpcao = this.opcao
+                var novaData = this.dataTarefa
 
                 if(idTarefa != -1){
                     this.tarefas.map(item => {
                         if(item.id == idTarefa){
                             item.status = novaOpcao
                             item.texto = novaTarefa
+                            item.data = novaData
                             item.isEditing = false
                         }
                         return item
                     }) 
-                }else{
-                    this.tarefas.push({id: id++, status: this.opcao, texto: this.novaTarefa, isEditing:false})
+                }else {
+                    this.tarefas.push({id: id++, status: this.opcao, texto: this.novaTarefa, data: this.dataTarefa , isEditing:false})
                 }               
                 this.idEditar = -1
             } 
             this.novaTarefa = ""
             this.styleSelect.display = "none"
+            this.dataVencimento()
+            this.ordenarTarefas()
         },
         
         editarTarefa(tarefa){
@@ -66,5 +75,37 @@ new Vue({
                 this.tarefas = this.tarefas.filter(i => i !== tarefa)
             }
         },
+
+        dataVencimento(){
+            let hoje = new Date()
+            let dia = hoje.getDate().toString().padStart(2,'0')
+            let mes = String(hoje.getMonth() + 1).padStart(2,'0')
+            let ano = hoje.getFullYear()
+            this.dataAtual = `${ano}-${mes}-${dia}` 
+        },
+
+        ordenarTarefas(){
+            var ordena = this.tarefas
+            ordena.sort(function (a,b){
+                if (a.status > b.status) {
+                    return 1;
+                }
+                if (a.status < b.status) {
+                    return -1;
+                }
+            });
+            return 0;
+        }
+        
+        // formataData(){
+        //     let ano = this.dataTarefa[0] + this.dataTarefa[1] + this.dataTarefa[2] + this.dataTarefa[3]
+        //     let mes = this.dataTarefa[5] + this.dataTarefa[6]
+        //     let dia = this.dataTarefa[8] + this.dataTarefa[9]
+        //     this.dataTarefa = `${dia}-${mes}-${ano}`
+        //     console.log(this.dataTarefa)
+        //     // let data = this.dataTarefa
+        //     // this.dataTarefa = moment(data).format('DD/MM/YYYY')
+
+        // }
     }
 })
