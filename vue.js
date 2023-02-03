0d1546d1ca76aa2f46101e0077ef3c208812aefb
Davi Ferreira <106997082+DaviF91@users.noreply.github.com>
2023-02-03 15:10:15 -0300

    projeto v11
    13- Permitir configurar limitação de caracteres do input

--- vue.js
+++ vue.js
@@ -5,23 +5,24 @@ Vue.component('input-text-counter',{
         return {
             limitarCaracter: "",
             limitar: [
-                {selecione: "Não"},
-                {selecione: "Sim"}
+                {selecione: "Sim"},
+                {selecione: "Não"}
             ]
             
              
         }
     },
     methods: {
-        
-       
+           
     },
     template:`
             <div>
+                <input type="checkbox" name="" id="">
                 <div class="template">
                     <label for="">Limitar descrição: </label>
-                    <select name="" v-model="limitarCaracter">
-                        <option v-for="(opcao,index) in limitar" :key="opcao">{{opcao.selecione}}</option>
+                    <select name="limitarCaracter"  v-model="limitarCaracter">
+                        <option disabled value="" >Selecione</option>
+                        <option v-for="(opcao,index) in limitar" :key="opcao" >{{opcao.selecione}}</option>
                     </select>
                 </div>
                 <div v-if="limitarCaracter == 'Sim'" class="template2">
@@ -30,7 +31,8 @@ Vue.component('input-text-counter',{
                         @input="$emit('input', $event.target.value)"  
                         placeholder="Nome da tarefa"
                         maxlength="100"
-                    ><span :class="{aceito: value.length <= 100, naoAceito: value.length > 100}">
+                    >
+                    <span :class="{valorAceito: value.length <= 100 && value.length >0  , valorNaoAceito: value.length > 100, valorPadrao: value.length == 0}">
                         {{value.length}}/100
                     </span>
                 </div>
@@ -41,7 +43,7 @@ Vue.component('input-text-counter',{
                         placeholder="Nome da tarefa"
                         maxlength=""
                     >
-                    <span :class="{aceito: value.length <= 100, naoAceito: value.length > 100}"> 
+                    <span :class="{valorAceito: value.length <= 100 && value.length >0 , valorNaoAceito: value.length > 100, valorPadrao: value.length == 0}"> 
                         {{value.length}}/100
                     </span>
                 </div>
@@ -88,7 +90,7 @@ new Vue({
                 
                 var novaTarefa = this.novaTarefa
                 var novaOpcao = this.opcao
-                var novaData = this.dataTarefa
+                var novaData = this.formataData(this.dataTarefa)
                 
                 if(idTarefa != -1){
                     this.tarefas.map(item => {
@@ -101,7 +103,7 @@ new Vue({
                         return item
                     }) 
                 }else {
-                    this.tarefas.push({nivel: 0, id: id++, status: this.opcao, texto: this.novaTarefa, data:  this.dataTarefa})
+                    this.tarefas.push({nivel: 0, id: id++, status: this.opcao, texto: this.novaTarefa, data: this.formataData(this.dataTarefa)})
                 }               
                 this.idEditar = -1
             } 
@@ -124,13 +126,24 @@ new Vue({
                 this.tarefas = this.tarefas.filter(i => i !== tarefa)
             }
         },
+
+        formataData(dataTarefa){
+            if(dataTarefa == ""){
+                return dataTarefa = ""
+            }
+            var dataInput  = dataTarefa
+            var data = new Date(dataInput)
+            return dataTarefa = data.toLocaleDateString('pt-BR', {timeZone: 'UTC'})
+        },
         
         dataVencimento(){
             let hoje = new Date()
-            let dia = hoje.getDate().toString().padStart(2,'0')
-            let mes = String(hoje.getMonth() + 1).padStart(2,'0')
-            let ano = hoje.getFullYear()
-            this.dataAtual = `${ano}-${mes}-${dia}` 
+            // let dia = hoje.getDate().toString().padStart(2,'0')
+            // let mes = String(hoje.getMonth() + 1).padStart(2,'0')
+            // let ano = hoje.getFullYear()
+            // this.dataAtual = `${dia}/${mes}/${ano}` 
+            this.dataAtual = hoje.toLocaleDateString('pt-BR', {timeZone: 'UTC'})
+            
         },
         
         ordenarTarefas(){
